33e9077cf08af42868ce81e143cbceb047ea6f29
BrahyanVergara <brahyanvergara955@gmail.com>
2026-03-16 22:22:48 -0500

    .gitignore creado en la raiz. Creacion de Token añadida y funcionamiento del Login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { login, register } from '../controllers/authController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+
+router.get("/profile", verifyToken, (req, res) => {
+    res.json({
+        message: "Ruta protegida",
+        user: req.user
+    });
+});
 
 export default router;
